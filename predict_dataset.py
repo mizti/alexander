7b7d3fd66e3b9ba0e9281b165525c32c7a947a42
keita shimizu <mizugokoro+sourcetree@gmail.com>
2017-01-17 16:29:16 +0900

    changed model npz name

--- predict_dataset.py
+++ predict_dataset.py
@@ -17,7 +17,8 @@ from image_dataset import *
 def predict(model, dataset):
     ans = []
     for index, item in enumerate(dataset):
-        ans.append(np.argmax(model(np.array([dataset.get_example(index)[0]])).data))
+        #ans.append(np.argmax(model(np.array([dataset.get_example(index)[0]])).data))
+        ans.append(np.argmax(model.predict(np.array([dataset.get_example(index)[0]])).data))
 
     return ans
 
@@ -26,7 +27,8 @@ def print_submit(ans):
         print("{0},{1}".format(index,ans[index]))
 
 if __name__ == '__main__':
-    model = CNN()
+    #model = CNN()
+    model = GoogLeNetBN()
     chainer.serializers.load_npz('GoogLeNet_epoch_60.npz', model)
     #trial_data = ImageDataset(normalize=True, flatten=False, max_size=224, dataselect=-1, mode='trial')
     trial_data = ImageDataset(normalize=True, flatten=False, max_size=224, dataselect=-1, mode='train')
