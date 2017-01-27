fcf1caff71c0bc1e3aacc99c84baa25c1bb91064
Keita Shimizu <miz@shimizukeita-no-MacBook.local>
2017-01-27 16:25:13 +0900

    used graph generator

--- draw_graph.py
+++ draw_graph.py
@@ -10,9 +10,17 @@ file_names = [
 ]
 
 data_names = [
-    'main/accuracy',
-    'validation/main/accuracy'
+    #'main/accuracy',
+    'validation/main/accuracy',
     #'main/loss',
+    "main/loss",
+    "main/loss1",
+    "main/loss2",
+    "main/loss3",
+    "validation/main/loss",
+    "validation/main/loss1",
+    "validation/main/loss2",
+    "validation/main/loss3"
 ]
 
 data = {}
@@ -22,7 +30,8 @@ for filename in file_names:
     #data[name] = json.load(f)
     js = json.load(f)
     for index, dataname in enumerate(data_names):
-        data[filename + '/' + dataname] = list(map(lambda x:x[dataname], js))
+        #data[filename + '/' + dataname] = list(map(lambda x:x[dataname], js))
+        data[dataname] = list(map(lambda x:x[dataname], js))
 
     f.close()
 
@@ -35,5 +44,5 @@ for name in data:
 
 plt.xlabel("epochs")
 plt.ylabel("accuracy")
-plt.legend(loc="best")
+plt.legend(loc="lower right")
 plt.savefig('result/graph.png', bbox_inches='tight')
